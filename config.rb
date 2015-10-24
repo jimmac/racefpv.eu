7fa5dcfda1a161d1977ba8229a04a6de30d64654
Jakub Steiner <jimmac@gmail.com>
2015-10-24 19:57:26 +0200

    blackout

--- config.rb
+++ config.rb
@@ -39,7 +39,7 @@ set :css_dir, 'css'
 set :js_dir, 'js'
 set :images_dir, 'images'
 set :layouts_dir, 'layouts'
-set :build_dir, 'build'
+set :build_dir, '../github.racefpv'
 
 
 activate :blog do |blog|
